4123d41d65af204a410b2652670f12cf25d4bd44
Martín España <46570760+martspain@users.noreply.github.com>
2022-09-12 20:23:37 -0600

    Initial Commit, added React template
    Added React template for basic web app. Implemented structure and basic loader files.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    mode: 'development',
+    devServer: {
+        static:'./dist'
+    },
+    module: {
+        rules: [
+            {
+              test: /\.(js|jsx)$/,
+              use: ['babel-loader'],
+            },
+            {
+              test: /\.css$/,
+              use: ['style-loader', 'css-loader'],
+            },
+            {
+              test: /\.(png|jpe?g|gif|svg|otf|mp4)$/i,
+              use: ['file-loader'],
+            },
+          ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+}

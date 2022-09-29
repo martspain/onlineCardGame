03d49fb9bc5b90a69be863be5cf653904fe7404d
Levi Tamath <tam19365@uvg.edu.gt>
2022-09-29 14:44:18 -0600

    Credits and Instructions
    add information on both screens

--- client/src/components/pages/Credits/credits.jsx
+++ client/src/components/pages/Credits/credits.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './credits.scss'
+import hi from '../../../assets/images/icons/hi.png'
 
 const Credits = () => {
     return(
@@ -7,19 +8,26 @@ const Credits = () => {
             <div className="title">Cre
                 <div class="title2">dits</div>
             </div>
-            <div className="containerText">
-                <div className="category">Developers
-                    <div className="line" />
+            <div className="containerDivision">
+                <div className="divisionLeft">
+                    <div className="containerText">
+                        <div className="category">Developers
+                            <div className="line" />
+                        </div>
+                        <li>Laura Tamath</li>
+                        <li>Martín España</li>
+                        <li>Sofia Rueda</li>
+                    </div>
+                    <div className="containerText">
+                        <div className="category">Inspiration
+                            <div className="line" />
+                        </div>
+                        <li>Club Penguin™</li>
+                    </div>
                 </div>
-                <li>Laura Tamath</li>
-                <li>Martín España</li>
-                <li>Sofia Rueda</li>
-            </div>
-            <div className="containerText">
-                <div className="category">Inspiration
-                    <div className="line" />
+                <div className="divisionRight">
+                    <img src={hi}  className="hi" alt="SayHi"  />
                 </div>
-                <li>Club Penguin™</li>
             </div>
         </div>
     )
--- client/src/components/pages/Instructions/Instructions.jsx
+++ client/src/components/pages/Instructions/Instructions.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import './Instructions.scss'
+import snowWater from '../../../assets/images/icons/snowVsWater.png'
+import fireSnow from '../../../assets/images/icons/fireVsSnow.png'
+import waterFire from '../../../assets/images/icons/waterVsFire.png'
 
 const Instructions = () => {
   return (
@@ -8,12 +11,18 @@ const Instructions = () => {
             <div class="title2">play</div>
         </div>
         <div className="instructionsText">
-            <ul>
-                <li>Take turns with your opponent to choose a card from the deck and start the battle.</li>
-                <li>Fire melts snow, water extinguishes fire, and snow freezes water.</li>
-                <li>The player who collects three cards of each element and of a different color, or three of the same element and of a different color is the one who wins the game.</li>
-                <li>Some powerful cards have special powers and nullify some of the opponent's moves.</li>
-            </ul>
+          <li> Create a new server, or join using the server creator code.</li>
+          <li> Start the game when all players are ready (games can be 2 - 4 people)</li>
+          <li> Chat with your friends by pressing the penguins button.</li>
+          <li> Each player must choose one of the 5 cards available to them.</li>
+          <li> Fire melts snow, water extinguishes fire, and snow freezes water.</li>
+          <div className="iconsContainerInstruc">
+            <img src={fireSnow}  className="imgVs" id="picture1" alt="FireVsSnow"  />
+            <img src={waterFire} className="imgVs" id="picture1" alt="WaterVsFire"  />
+            <img src={snowWater} className="imgVs" id="picture1" alt="SnowVsWater"  />
+          </div>
+          <li> The player who collects three cards of each element wins the game.</li>
+
         </div>
     </div>
   )
